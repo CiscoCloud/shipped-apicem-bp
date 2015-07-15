087764440d7c6ba2a978d5180e4bd7c14d6022ed
David Tootill <github@tootill.net>
2015-07-15 14:37:12 -0700

    Refactor folder structure and add server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+/*
+Server for Shipped CMX demo project
+
+Author: David Tootill 2015.07.15
+Copyright (C) Cisco, Inc.  All rights reserved.
+*/
+
+var express     = require("express");
+var bodyParser  = require("body-parser")
+var serveStatic = require("serve-static")
+var compression = require("compression")
+
+var nconf = require("nconf");
+nconf.argv()
+     .file({file: __dirname + "/config.json"})
+
+// Set up middleware to serve UI static content
+
+var app = express();
+app.use(compression())
+app.use(serveStatic(__dirname + "/../ui")) 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*")
+  next()
+})
+
+// Start the server
+app.listen(nconf.get("port"))
+console.log("Server is listening on port " + nconf.get("port"))
